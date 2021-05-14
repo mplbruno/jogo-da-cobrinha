62411e33b235d5edbfe9cfbd1739762bdadc4717
mplbruno <mplbruno@gmail.com>
2021-05-13 21:19:40 -0300

    Impedir que a "cobrnha" suma

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ let context =canvas.getContext("2d");
 let box = 32;
 let snake = [];
 snake[0] = {
-    x: 8 * box,
-    y: 8 * box,
-} 
+    x:8 * box,
+    y:8 * box
+}; 
 let direction = "right";
-
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 function criarBG() {
     context.fillStyle = "lightgreen";
-    context.fillRect(0,0,16 * box, 16 * box);
+    context.fillRect(0, 0 , 16 * box, 16 * box);
 }
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
@@ -18,8 +21,26 @@ function criarCobrinha(){
         context.fillRect(snake[i].x, snake[i].y, box,box);
     }
 }
+function drawFood(){
+    context.fillStyle = "green";
+    context.fillRect(snake[i].x, snake[i].y, box,box);
+}
+
+document.addEventListener('keydown' , update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
 
 function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
     
@@ -29,13 +50,13 @@ function iniciarJogo(){
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
-    if(direction == "down") snakeX += box;
+    if(direction == "down") snakeY += box;
 
     snake.pop();
 
     let newHead = {
         x: snakeX,
-        y: snakeY,
+        y: snakeY
     }
 
     snake.unshift(newHead);
